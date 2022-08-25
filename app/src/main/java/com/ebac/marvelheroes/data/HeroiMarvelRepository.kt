8de6accb5d2fb7348c42b65e58e5c26d9b8542a5
Nadilson <nadilson@protonmail.com>
2022-08-25 04:41:09 -0300

    BuscaActivity, buscarHeroiPorNome() em HeroiMarvelRepository

--- app/src/main/java/com/ebac/marvelheroes/data/HeroiMarvelRepository.kt
+++ app/src/main/java/com/ebac/marvelheroes/data/HeroiMarvelRepository.kt
@@ -1,5 +1,9 @@
 package com.ebac.marvelheroes.data
 
+import com.ebac.marvelheroes.data.model.HeroiResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,6 +12,28 @@ class HeroiMarvelRepository {
         .baseUrl("https://gateway.marvel.com:443")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+
+    fun buscarHeroiPorNome(nome: String, sucesso: (HeroiResponse) -> Unit, erro: () -> Unit){
+        val api = retrofit.create(MarvelAPI::class.java)
+
+        api.buscarHeroiPorNome(nome, KEY, TIMESTAMP, HASH).enqueue(
+            object: Callback<HeroiResponse> {
+                override fun onResponse(call: Call<HeroiResponse>?, response: Response<HeroiResponse>?) {
+                    if (response?.isSuccessful == true) {
+                        sucesso(response.body())
+                    } else {
+                        erro()
+                    }
+                }
+
+                override fun onFailure(call: Call<HeroiResponse>?, t: Throwable?) {
+                    erro()
+                }
+
+            }
+        )
+    }
 
     companion object{
         val KEY = "YOUR_MARVEL_DEV_KEY_HERE"
--- app/src/main/java/com/ebac/marvelheroes/presentation/view/BuscaActivity.kt
+++ app/src/main/java/com/ebac/marvelheroes/presentation/view/BuscaActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import com.ebac.marvelheroes.R
 import com.ebac.marvelheroes.data.HeroiMarvelRepository
 import com.ebac.marvelheroes.databinding.ActivityBuscaBinding
+
 class BuscaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBuscaBinding
@@ -23,14 +24,15 @@ class BuscaActivity : AppCompatActivity() {
 
         binding.botaoPesquisar.setOnClickListener {
             val text = binding.barraPesquisa.text.toString()
-            val duration = Toast.LENGTH_SHORT
-
-            val toast = Toast.makeText(applicationContext, text, duration)
-            toast.show()
-
 
             repository.buscarHeroiPorNome(text,
-                {},
+                {
+                    val texto2 = it.data.results[0].description
+                    val duration = Toast.LENGTH_SHORT
+
+                    val toast = Toast.makeText(applicationContext, texto2, duration)
+                    toast.show()
+                },
                 {}
             )
         }
